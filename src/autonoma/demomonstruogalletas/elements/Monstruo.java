1dc514b65ca62de55732e0bb141def3964fa2389
Heily Rios <heilyayala005@gmail.com>
2025-05-23 22:47:02 -0500

    Se crearon nuevas clases y paquetes

---
+++ src/autonoma/demomonstruogalletas/elements/Monstruo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package autonoma.demomonstruogalletas.elements;
+
+/**
+ * 
+ * @author Heily Yohana Rios Ayala <heilyy.riosa@autonoma.edu.co>
+ * @since 20250523
+ * @version 1.0.0
+ */
+public class Monstruo {
+    
+}
